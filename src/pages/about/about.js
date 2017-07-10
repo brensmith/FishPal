2a799e131857ae7f121a458ec2d383787877c45a
Brendan Smith <brendansmith82@hotmail.com>
2017-07-10 18:24:40 +0100

    Created basic componants for about and contact pages

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,1 +1,14 @@
-about.js
+import React, { Component } from 'react';
+import './about.css';
+
+class about extends Component {
+  render() {
+    return (
+      <div className="about">
+        <h1>About Page</h1>
+      </div>
+    );
+  }
+}
+
+export default about;
--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,1 +1,14 @@
-contact.js
+import React, { Component } from 'react';
+import './contact.css';
+
+class contact extends Component {
+  render() {
+    return (
+      <div className="contact">
+        <h1>contact Page</h1>
+      </div>
+    );
+  }
+}
+
+export default contact;
