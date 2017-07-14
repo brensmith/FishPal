35cd40c7b1611d5e3e5a7c5514f0fce9c94c7d6f
Brendan Smith <brendansmith82@hotmail.com>
2017-07-14 21:00:20 +0100

    Created content for About page

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -7,7 +7,7 @@ class about extends Component {
   render() {
     return (
 	      	<div className="about">
-		        <div className="container">
+		    <div className="container">
 			        <div className="row">
 			            <div className="col-md-10">
 			                <h1 className="page-header">About <small>FishPal React SPA. </small></h1>
@@ -15,27 +15,27 @@ class about extends Component {
 				            		
 	                	</div>
 		        	</div>
-		        	<div className="row">
-			            <div className="col-md-10">
+		        	<div className="row justify-content-start">
+			            <div className="col-sm-4">
 			            	<p><small>(Figure 1.)</small> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, explicabo dolores ipsam aliquam inventore corrupti eveniet quisquam quod totam laudantium repudiandae obcaecati ea consectetur debitis velit facere nisi expedita vel?</p>
 	                				<img className="img-round img-responsive img-right" src="http://placehold.it/400x200" alt="Figure 1"></img>
 	                	</div>
-		        	</div>
-		        	<div className="row">
-			            <div className="col-md-10">
+		        	
+		        	
+			            <div className="col-sm-4">
 			            	<p><small>(Figure 2.)</small> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, explicabo dolores ipsam aliquam inventore corrupti eveniet quisquam quod totam laudantium repudiandae obcaecati ea consectetur debitis velit facere nisi expedita vel?</p>
 	                				<img className="img-round img-responsive img-right" src="http://placehold.it/400x200" alt="Figure 2"></img>
 	                	</div>
-		        	</div>
-		        	<div className="row">
-			            <div className="col-md-10">
+		        	
+		        	
+			            <div className="col-sm-4">
 			            	<p><small>(Figure 3.)</small> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, explicabo dolores ipsam aliquam inventore corrupti eveniet quisquam quod totam laudantium repudiandae obcaecati ea consectetur debitis velit facere nisi expedita vel?</p>
 	                				<img className="img-round img-responsive img-right" src="http://placehold.it/400x200" alt="Figure 3"></img>
 	                	</div>
 		        	</div>
 		        	<hr></hr>
 
-        		</div>
+        		</div>    
         	</div>
     );
   }
