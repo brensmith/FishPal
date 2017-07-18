7ab41c30dd35fb507d0ebf5e043c924ff6a55699
Brendan Smith <brendansmith82@hotmail.com>
2017-07-18 20:43:21 +0100

    Created Add Fish and display Fish object functionallity

--- src/pages/addfish/addfish.js
+++ src/pages/addfish/addfish.js
@@ -1,24 +1,149 @@
 import React, { Component } from 'react';
-import './addfish.css';
+import { browserHistory } from 'react-router';
+var FId ;
+
+class AddFish extends Component {
+    
+  constructor(props) {
+        var datacheck = JSON.parse(localStorage.getItem('fishes'));
+        if (datacheck === null || datacheck.length === 0)
+            {
+            FId = 0;
+            }
+        else
+            {
+          FId = (JSON.parse(localStorage.getItem('fishes'))[JSON.parse(localStorage.getItem('fishes')).length - 1].id) + 1;
+            }
+
+    super(props);
+        
+    this.state = {
+      fishes: JSON.parse(localStorage.getItem('fishes')) || [],
+      newFish: {
+            id: FId,
+            species: "Species",
+            weight: "Weight",
+            location: "Location",
+            description: "Description",
+            
+       },
+             
+    };
+        
+    this.submitFish = this.submitFish.bind(this);
+    
+        
+  }
+    
+  submitFish() {
+    console.log('Submit Fish');
+    console.log(this.species.value, this.weight.value, this.location.value, this.description.value);
+        if (this.species.value !== '' && this.weight.value !== '' && this.location.value !== '' && this.description.value !== '' )
+            {
+    let newFish = this.state.newFish;
+        newFish.species = this.species.value;
+        newFish.weight = this.weight.value;
+        newFish.location = this.location.value;
+        newFish.description = this.description.value;
+        
+
+    this.setState({newFish});
+
+    let fishes = this.state.fishes;
+    fishes.push(newFish);
+
+    this.setState({fishes});
+    localStorage.setItem('fishes', JSON.stringify(fishes));
+    console.log(fishes);
+    browserHistory.push('/');
+                }
+        else
+            {
+                console.log("Attention")
+                alert("Please Fill The Required Fields");
+
+            }
+
+  }
 
 
-class addfish extends Component {
-  render() {
+    render() {
+            
+            
     return (
 
-    	<div className="addfish">
-    		<div className="container">
-			        <div className="row">
-			            <div className="col-md-10">
-			                <h1 className="page-header">Add Fish <small>FishPal React SPA. </small></h1>
-			                	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, explicabo dolores ipsam aliquam inventore corrupti eveniet quisquam quod totam laudantium repudiandae obcaecati ea consectetur debitis velit facere nisi expedita vel?</p>
-      					</div>
-  					</div>
-			</div>
-		</div>
+        <div className="container">
+              <div className="row">
+                  <div className="col-md-10">
+                      <h2 className="page-header">Add a Fish</h2>
+                        <p>A Species of Fish can be added to the database by filling in the form below with the required data.</p>
+                        
+                    </div>
+              
+      <div className="row">
+        <div className="col-md-6" id="fishform">
+          <form>
+            <br>
+            </br>                      
+            <div className="form-group">
+               <label htmlFor="Species">Species</label>
+                  <select id="species" className="form-control" ref={(input)=> this.species = input}>
+                      <option value="Select">Select</option>
+                      <option value="Atlantic cod">Atlantic cod</option>
+                      <option value="Bream">Bream</option>
+                      <option value="Bass">Bass</option>
+                      <option value="Barbel">Barbel</option>
+                      <option value="Brown trout">Brown trout</option>
+                      <option value="Conger Eel">Conger Eel</option>
+                      <option value="Dace">Dace</option>
+                      <option value="Dog Fish">Dog Fish</option>
+                      <option value="Eel">Eel</option>
+                      <option value="Flounder">Flounder</option>
+                      <option value="Mackeral">Mackeral</option>
+                      <option value="Pike">Pike</option>
+                      <option value="Rainbow Trout">Rainbow Trout</option>
+                      <option value="Other">Other...</option>
+                  </select>
+            </div>
+
+
+            <div className="form-group">
+              <label htmlFor="Weight">Weight</label>
+                    <input type="number" 
+                            ref={(input) => {this.weight = input;}}
+                            className="form-control" 
+                            id="weight" 
+                            placeholder="Enter Weight" />
+            </div>    
+
+            <div className="form-group">
+              <label htmlFor="location">Your Location</label>
+                    <input type="text" 
+                            ref={(input) => {this.location = input;}}
+                            className="form-control" 
+                            id="location" 
+                            placeholder="Enter your Location" />
+            </div>
+                    
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+                    <textarea className="form-control" 
+                    id="description" 
+                    ref={(input) => {this.description = input;}}
+                    placeholder="Enter a description" />
+            </div>
+
+            <button type="button" onClick={this.submitFish} className="btn btn-default">Submit</button>
+          </form>
+          <hr/>
+      </div>
+      </div>
+</div>
+      </div>
 
     );
   }
 }
 
-    export default addfish;
+
+export default AddFish;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,24 +1,72 @@
 import React, { Component } from 'react';
 import './home.css';
 
-
 class Home extends Component {
+
+  constructor(props) {
+
+    super(props);
+    
+        this.state = {
+        fishes: JSON.parse(localStorage.getItem('fishes')) || [],
+        }
+            var fishescheck = JSON.parse(localStorage.getItem('fishes'));
+              if (fishescheck === null || fishescheck.length === 0)
+                {
+                    let fishes = this.state.fishes;
+                    this.setState({fishes});
+                    localStorage.setItem('fishes', JSON.stringify(fishes));
+                }
+                  
+                else{
+                      
+                    }
+
+  }    
+    
+  displayFish() {
+                  let resultsArray = [];
+                    this.state.fishes.map((fish, i) => {
+                      resultsArray.push( 
+                            <div key={fish.id} className="row justify-content-start">
+                                  <div className="col-md-6">
+                                      <p>Species :{fish.species}</p>
+                                      <p>Weight in lbs :{fish.weight}</p>
+                                      <p>Location Caught :{fish.location}</p>
+                                      <p>Description :{fish.description}</p>
+                                      <hr></hr>
+                                  </div>
+                            </div>
+                      );
+                  });
+                  return resultsArray; 
+
+                  }
+
+    
+
   render() {
     return (
+      <div className="home">
+          <div className="container">
+              <div className="row">
+                  <div className="col-md-12">
+                      <h1 className="page-header">FishPal<small> The Fishermans Database. </small></h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, explicabo dolores ipsam aliquam inventore corrupti eveniet quisquam quod totam laudantium repudiandae obcaecati ea consectetur debitis velit facere nisi expedita vel?</p>
+                        
+                    </div>
+              </div>
+            
+            
+            <br></br>
+            {this.displayFish()}
 
-    	<div className="home">
-    		<div className="container">
-			        <div className="row">
-			            <div className="col-md-10">
-			                <h1 className="page-header">Home Page <small>FishPal React SPA. </small></h1>
-			                	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, explicabo dolores ipsam aliquam inventore corrupti eveniet quisquam quod totam laudantium repudiandae obcaecati ea consectetur debitis velit facere nisi expedita vel?</p>
-      					</div>
-  					</div>
-			</div>
-		</div>
-
+            
+           </div>  
+      </div>
+           
     );
-    }
-    }
+  }
+}
 
-    export default Home;
+export default Home;
